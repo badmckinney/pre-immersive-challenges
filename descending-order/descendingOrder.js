f663c8cc5ad3ee75efe5fa9a19b5431b79e3ada3
Brad McKinney <brad.mckin@gmail.com>
2018-12-11 22:00:48 -1000

    completed descendingOrder challenge

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,3 +1,14 @@
 module.exports = function descendingOrder(number) {
-  // write code in here
+  if (typeof number === 'number') {
+    let numStr = number.toString();
+    numStrArr = [];
+    for(i = 0; i < numStr.length; i++) {
+      numStrArr.push(numStr[i]);
+    }
+    numStrArr.sort().reverse();
+    newNumber = numStrArr.join('');
+    return parseInt(newNumber);
+  } else {
+    return "not a number!"
+  }
 }
